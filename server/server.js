c5eb8bf54675b34005949aecd02f55b8c2c05710
Kevin Dial <kevin.dial.phone@gmail.com>
2016-01-16 21:37:23 -0500

    trying to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,73 +1,20 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var CLIENTS = [];
+app.set('port', (process.env.PORT || 5000));
 
-var server = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
-});
-server.listen(1337, function() {
-    console.log("### LISTENING ON PORT 1337.");
-});
+app.use(express.static(__dirname + '/public'));
 
-// create the server
-wsServer = new WebSocketServer({
-    httpServer: server
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-// WebSocket server
-wsServer.on('request', function(request) {
-    var connection = request.accept(null, request.origin);
-
-    // This is the most important callback for us, we'll handle
-    // all messages from users here.
-    connection.on('message', function(message) {
-        console.log("### PROCESS MESSAGE:", message);
-        if (message.type === 'utf8') {
-            // process WebSocket message
-            try {
-                var json = JSON.parse(message.utf8Data);
-            } catch (e) {
-                console.log('This doesn\'t look like a valid JSON: ', message.data);
-                return;
-            }
-
-            console.log("Message JSON:", json);
-
-            if (json.message === 'connected') {
-                var id = CLIENTS.length;
-                sendJSON(connection, {
-                    "id" : id,
-                    "message" : "connection accepted"
-                });
-                CLIENTS.push(connection);
-                console.log("Connection from user " + id + " accepted.");
-            } else if (json.message === 'ping') {
-                console.log("Ponging the ping.");
-                sendMessage(connection, "pong")
-            } else if (json.message === 'play') {
-                console.log("Broadcasting play message to " + CLIENTS.length + " clients.");
-                for (var i = 0; i < CLIENTS.length; i++) {
-                    sendMessage(CLIENTS[i], "playing");
-                }
-            }
-
-        }
-    });
-
-    connection.on('close', function(connection) {
-        // close user connection
-        console.log("### USER CONNECTION CLOSED.");
-    });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-// helpers
 
-function sendJSON(connection, json_obj) {
-    connection.send(JSON.stringify(json_obj));
-}
-
-function sendMessage(connection, msg) {
-    sendJSON(connection, {"message" : msg});
-}
