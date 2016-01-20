4a4b2ff772712423b7676704a082a8dc164687f6
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-01-20 01:57:53 -0500

    Added drag listener to progress bar and cursor

--- server/client/progress_bar.js
+++ server/client/progress_bar.js
@@ -19,14 +19,45 @@ window.onload = function(){
 
   var cursor = document.getElementById('position-cursor');
 
-  $( ".position" ).click(function(event){
-    console.log(event.offsetX);
+  var pressed = false;
+  $( ".position" )
+  .mousedown(function(event) {
+    console.log("MOUSEDOWN:", event, event.offsetX);
+    pressed = true;
     var position = event.offsetX;
     var total = player.seekable.end(0);
     var timePosition = position * total / 300;
 
     player.currentTime = parseInt(timePosition);
     cursor.style.marginLeft = String(position - 5)+"px";
+  })
+  .mousemove(function(event) {
+    if (pressed){
+      console.log("MOUSEMOVE:",event , event.offsetX);
+      var position = event.offsetX;
+      var total = player.seekable.end(0);
+      var timePosition = position * total / 300;
 
-  });
+      player.currentTime = parseInt(timePosition);
+      cursor.style.marginLeft = String(position - 5)+"px";
+    }
+  })
+  .mouseup(function(event) {
+    if (pressed){
+      pressed = false;
+      // console.log("MOUSEUP:",event);
+    }
+  })
+
+  // $( ".position" )
+  // .click(function(event){
+  //   console.log(event.offsetX);
+  //   var position = event.offsetX;
+  //   var total = player.seekable.end(0);
+  //   var timePosition = position * total / 300;
+
+  //   player.currentTime = parseInt(timePosition);
+  //   cursor.style.marginLeft = String(position - 5)+"px";
+
+  // });
 }
